e95bc4b4ba7b5d6b8243db3f0be6f0c73c12445c
d.takeuchi <d.takeuchi@technoplan-inc.co.jp>
2015-09-15 09:18:56 +0900

    テストロジックの追加

--- application/tests/models/News_model_test.php
+++ application/tests/models/News_model_test.php
@@ -30,12 +30,35 @@ class News_model_test extends TestCase
      */
     public function ニュースを全件取得()
     {
+        $expected = 3;
         $limit = array(
             'limit' => 10,
             'offset' => 0
         );
         $actual = $this->obj->get_news(FALSE, $limit);
-        $this->assertEquals(3, count($actual));
+        $this->assertEquals($expected, count($actual));
     }
 
+    /**
+     * @test
+     */
+    public function ニュースの件数を取得()
+    {
+        $expected = 3;
+        $actual = $this->obj->get_count_all();
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function ニュースを新規登録する()
+    {
+        $news = array(
+            'title' => '単体テスト_title',
+            'text' => '単体テスト_text',
+            'slug' => url_title('単体テスト_title', 'dash', TRUE),
+        );
+        $this->obj->save($news);
+    }
 }
