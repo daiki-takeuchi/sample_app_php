fedeae8c068e641b0106a58aa1bc974b3f81fd44
takeuchi <d.takeuchi@technoplan-inc.co.jp>
2015-09-17 11:22:27 +0900

    datebase設定を環境ごとに変えるよう修正。 合わせてPHPUnitでも動くよう修正

--- application/tests/TestCase.php
+++ application/tests/TestCase.php
@@ -2,4 +2,24 @@
 
 class TestCase extends CIPHPUnitTestCase
 {
+    protected static $databaseSetup = false;
+
+    public function __construct()
+    {
+        $this->setUpDatabase();
+    }
+
+    protected function setUpDatabase()
+    {
+        if (static::$databaseSetup) {
+            return;
+        }
+        $this->CI =& get_instance();
+        $this->CI->load->library('migration');
+        if ( ! $this->CI->migration->current())
+        {
+            $this->CI->migration->latest();
+        }
+        static::$databaseSetup = true;
+    }
 }
